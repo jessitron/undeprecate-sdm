d261c7a3d2eba2807f259cc53c87b588ff1adaa2
Jessica Kerr <jessitron@atomist.com>
2019-01-29 15:32:15 -0600

    Simplest possible transform, eh?

--- lib/transform/deprecatedMethod/byRegex.ts
+++ lib/transform/deprecatedMethod/byRegex.ts
@@ -5,9 +5,6 @@ export function changeDeprecatedMethodWithRegex(params: {
     deprecatedMethodName: string,
     replacementMethodName: string,
 }): CodeTransform {
-    return async project => {
-        await doWithFiles(project, "**/*.java", async f => {
-            await f.replaceAll(params.deprecatedMethodName, params.replacementMethodName);
-        });
-    };
+    return project => doWithFiles(project, "**/*.java",
+        f => f.replaceAll(params.deprecatedMethodName, params.replacementMethodName));
 }
