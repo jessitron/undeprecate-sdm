94f4972ae0cdb1c5add301ad80e41dc887deb698
Jessica Kerr <jessitron@atomist.com>
2019-02-01 12:30:03 -0800

    Looks like automation-client had a useful fix

--- lib/transform/java/index.ts
+++ lib/transform/java/index.ts
@@ -1,26 +1,29 @@
 import { Java9FileParser } from "@atomist/antlr";
 import { astUtils, Project, ProjectFile } from "@atomist/automation-client";
-import { evaluateExpression, isSuccessResult, TreeNode } from "@atomist/tree-path";
 import { matchIterator } from "@atomist/automation-client/lib/tree/ast/astUtils";
+import { evaluateExpression, isSuccessResult, TreeNode } from "@atomist/tree-path";
 
-export async function hasImport(importName: string, file: ProjectFile): Promise<boolean> {
+export async function hasImport(importName: string, p: Project, path: string): Promise<boolean> {
     // we could also check for a static, but I think that'd be separate
-    const parsed = await Java9FileParser.toAst(file);
-
-    return astHasImport(importName, parsed);
-}
-
-function astHasImport(importName: string, javaAST: TreeNode): boolean {
     const specificPackageImport =
         `/compilationUnit//importDeclaration//typeName[@value='${importName}']`;
-    const specificMatch = evaluateExpression(javaAST, specificPackageImport);
-    if (isSuccessResult(specificMatch)) {
+
+    const it = await matchIterator(p, {
+        globPatterns: path,
+        pathExpression: specificPackageImport,
+        parseWith: Java9FileParser,
+    });
+    for await (const m of it) {
         return true;
     }
 
     const dotStarImport = `/compilationUnit//importDeclaration[//MUL]//typeName[@value='${dropClassName(importName)}']`;
-    const starMatch = evaluateExpression(javaAST, dotStarImport);
-    if (isSuccessResult(starMatch)) {
+    const it2 = await matchIterator(p, {
+        globPatterns: path,
+        pathExpression: dotStarImport,
+        parseWith: Java9FileParser,
+    });
+    for await (const m of it2) {
         return true;
     }
 
@@ -55,12 +58,13 @@ export async function addImport(p: Project, importName: string, file: ProjectFil
         parseWith: Java9FileParser,
     });
     for await (const m of it) {
-       // console.log(JSON.stringify(m));
+        // console.log(JSON.stringify(m));
         m.$value = `import ${importName};\n${m.$value}`;
-        console.log("Doing magic: value=" + m.$value)
+        console.log("Doing magic: value=" + m.$value);
 
-        break;
+        //  break;
     }
+    (p as any).flush(); // still doesn't help
 }
 
 function dropClassName(qualifiedClass: string): string {
