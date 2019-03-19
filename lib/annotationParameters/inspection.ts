34d88a53baac51c354b9dcfe1cfc42adc508bd39
Jessica Kerr <jessitron@atomist.com>
2019-03-18 22:04:02 -0500

    Implement

--- lib/annotationParameters/inspection.ts
+++ lib/annotationParameters/inspection.ts
@@ -1,11 +1,51 @@
-import { Project, ProjectReview } from "@atomist/automation-client";
+import { Java9FileParser } from "@atomist/antlr";
+import { astUtils, Project, ProjectReview, ReviewComment } from "@atomist/automation-client";
+import { PatternMatch } from "@atomist/microgrammar/lib/PatternMatch";
 import { CodeInspection } from "@atomist/sdm";
+
+interface AnnotationAstNode {
+    normalAnnotation?: {
+        elementValuePairList: PatternMatch & {
+            elementValuePairs: Array<{
+                identifier: { Identifier: string },
+                elementValue: PatternMatch,
+            }>,
+        },
+    };
+    singleElementAnnotation?: {
+        elementValue: PatternMatch,
+    };
+}
 
 export function lookAtAnnotationParameters(params:
     {
         annotationName: string,
     }): CodeInspection<ProjectReview> {
     return async (p: Project) => {
-        return { repoId: p.id, comments: [] };
+
+        const matches = await astUtils.findMatches<AnnotationAstNode>(
+            p,
+            Java9FileParser,
+            "src/main/java/**/*Application.java", `//annotation[//identifier[@value='${params.annotationName}']]`,
+        );
+
+        const comments: ReviewComment[] = matches.map(m => {
+            let detail;
+            if (m.normalAnnotation) {
+                detail = m.normalAnnotation.elementValuePairList.$value;
+            } else if (m.singleElementAnnotation) {
+                detail = "value = " + m.singleElementAnnotation.elementValue.$value;
+            } else {
+                detail = "no parameters";
+            }
+            const c: ReviewComment = {
+                detail,
+                severity: "info",
+                category: params.annotationName,
+            };
+            return c;
+        });
+
+        return { repoId: p.id, comments };
     };
 }
--- test/annotationParameters/inspection.test.ts
+++ test/annotationParameters/inspection.test.ts
@@ -2,35 +2,54 @@ import { InMemoryProject } from "@atomist/automation-client";
 import * as assert from "assert";
 import { lookAtAnnotationParameters } from "../../lib/annotationParameters/inspection";
 
-const usefulJava = `
+function usefulJava(paramsInParens: string) {
+    return `
 package com.jessitron.hg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {}, excludeName = { "exclude" })
+@SpringBootApplication${paramsInParens}
 public class HorseguardsApplication {
 
+    @ThisIsNotTheOneYouWant("these are bad")
 	public static void main(String[] args) {
 		SpringApplication.run(HorseguardsApplication.class, args);
 	}
 }
 `;
-const realisticName = "src/main/com/jessitron/hg/HorseguardsApplication.java";
+}
+const realisticName = "src/main/java/com/jessitron/hg/HorseguardsApplication.java";
 
 describe("the annotation parameters inspection", () => {
     it("finds no results if the annotation doesn't exist", async () => {
-        const input = InMemoryProject.of({ path: realisticName, content: usefulJava });
+        const input = InMemoryProject.of({ path: realisticName, content: usefulJava(`(exclude = {}, excludeName = { "exclude" })`) });
         const result = await
             lookAtAnnotationParameters({ annotationName: "NotHere" })(input, {} as any);
         assert.strictEqual(result.comments.length, 0);
     });
 
     it("finds a result for an annotation that exists", async () => {
-        const input = InMemoryProject.of({ path: realisticName, content: usefulJava });
+        const input = InMemoryProject.of({ path: realisticName, content: usefulJava(`(exclude = {}, excludeName = { "exclude" })`) });
         const result = await
             lookAtAnnotationParameters({ annotationName: "SpringBootApplication" })(input, {} as any);
         assert.strictEqual(result.comments.length, 1);
         assert.strictEqual(result.comments[0].detail, `exclude = {}, excludeName = { "exclude" }`);
+    });
+
+    it("finds an annotation with no parameters that exists", async () => {
+        const input = InMemoryProject.of({ path: realisticName, content: usefulJava("") });
+        const result = await
+            lookAtAnnotationParameters({ annotationName: "SpringBootApplication" })(input, {} as any);
+        assert.strictEqual(result.comments.length, 1);
+        assert.strictEqual(result.comments[0].detail, `no parameters`);
+    });
+
+    it("finds an annotation with only a value parameter", async () => {
+        const input = InMemoryProject.of({ path: realisticName, content: usefulJava("(4)") });
+        const result = await
+            lookAtAnnotationParameters({ annotationName: "SpringBootApplication" })(input, {} as any);
+        assert.strictEqual(result.comments.length, 1);
+        assert.strictEqual(result.comments[0].detail, `value = 4`);
     });
 });
